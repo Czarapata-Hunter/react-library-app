eb285d5fd01ec3a10973e00bf2bdcf20a51534e0
Hunter Czarapata <czarapata.hunter@gmail.com>
2023-01-23 17:01:00 -0800

    Added link to main landing page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail.js';
+import { Link } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <Switch>
+        <Route exact path={'/'}>
+          <h1> Book Catalog</h1>
+          <Link to="/books">Book List</Link>
+        </Route>
         <Route exact path="/books" component={BookList} />
         <Route path="/books/:id" component={BookDetail} />
       </Switch>
